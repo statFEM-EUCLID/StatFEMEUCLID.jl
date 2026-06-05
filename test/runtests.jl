43cb9af52a80c61c07c7f1c9ef18b9ed01f72672
Simon Christ <simonchrist@gmx.de>
2026-06-05 10:03:26 +0200

    formatting and tuple to array

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,28 +19,32 @@ end
 
 @testset "StatFEMEUCLID.jl" begin
     @testset "1DBar Example" begin
+        # runic: off
 ##
+        # runic: on
         N = 50
-        function solution(E, F = 800, A = 20)
-            return [LinRange( 0, 100, N) *  F / (A * E)]
+        function solution(E, F=800, A=20)
+            return [LinRange(0, 100, N) * F / (A * E)]
         end
         p1 = @patch UMBridge.evaluate(model, input, config=Dict()) = solution(only(input))
-        p2 = @patch UMBridge.model_output_sizes(model::HTTPModel, config = Dict()) = (N,)
+        p2 = @patch UMBridge.model_output_sizes(model::HTTPModel, config=Dict()) = (N,)
 
         rng = MersenneTwister(2020)  #fixed seed for comparability between runs
         μ_E = 200.0
         σ_E = 10.0
         n_MonteCarlo = 1000
         server_url = "http://localhost:4343"
-    
+
         fem_model = UMBridge.HTTPModel("Bar1D.FEM", server_url)
-        
+
         lognormal_dist = create_lognormal_distribution(μ_E, σ_E)
-        sample_MC = apply((p1, p2)) do
-            StatFEMEUCLID.Sampling.sample_FEM(fem_model, n_MonteCarlo, sample_distribution = lognormal_dist, rng = rng)
+        sample_MC = apply([p1, p2]) do
+            StatFEMEUCLID.Sampling.sample_FEM(fem_model, n_MonteCarlo, sample_distribution=lognormal_dist, rng=rng)
         end
         mu, _ = StatFEMEUCLID.Sampling.compute_statistics(sample_MC)
-        @test isapprox(mu[end], 20, atol = 0.05) 
+        @test isapprox(mu[end], 20, atol=0.05)
+        # runic: off
 ##
+        # runic: on
     end
 end
